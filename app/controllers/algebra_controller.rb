90ae9301f20c8610670e988ecfb8da48f4b98dff
javymeza10 <mezajavier1833@gmail.com>
2021-05-16 20:59:17 -0700

    CSS/HTML/RB Updates

--- app/controllers/algebra_controller.rb
+++ app/controllers/algebra_controller.rb
@@ -1,4 +1,14 @@
 class AlgebraController < ApplicationController
   def AlgResults
+    #puts @sum1 + @sum2
   end
+
+  def self.AlgResults(a,b)
+    a.to_i + b.to_i
+  end
+
+  def add
+    @result = AlgebraController.send(params[:operation], *[params[:a], params[:b]])
+  end
+
 end
